0f2d4547f619c57625915b3d4d6ac5784ea9b395
structsize <52183019+structsize@users.noreply.github.com>
2021-06-15 21:47:51 +0900

    feat: create sign up activity

---
+++ app/src/main/java/org/software/meister/gsm/singit/view/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package org.software.meister.gsm.singit.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.software.meister.gsm.singit.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
