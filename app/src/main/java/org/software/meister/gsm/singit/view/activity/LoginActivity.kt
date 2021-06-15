b9158e66c11827381ba262f5ab1c3b509657a2d8
structsize <52183019+structsize@users.noreply.github.com>
2021-06-15 16:38:30 +0900

    feat: create login activity

---
+++ app/src/main/java/org/software/meister/gsm/singit/view/activity/LoginActivity.kt
@@ -0,0 +1,12 @@
+package org.software.meister.gsm.singit.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.software.meister.gsm.singit.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
